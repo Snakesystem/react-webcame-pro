6e7dc153110c1c4c0eec4e2ba6723be6ba1e196c
feryirawan <ir15y4hh@gmail.com>
2024-08-13 17:17:31 +0700

    Ubah style

--- src/components/CameraPopup.jsx
+++ src/components/CameraPopup.jsx
@@ -6,26 +6,11 @@ const CameraPopup = ({ isOpen, onClose }) => {
   const popupRef = useRef(null);
   const webcamRef = useRef(null);
   const [image, setImage] = useState(null);
-  const [orientation, setOrientation] = useState('portrait');
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImage(imageSrc);
   };
-
-  const handleOrientationChange = () => {
-    const isPortrait = window.innerHeight > window.innerWidth;
-    setOrientation(isPortrait ? 'portrait' : 'landscape');
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleOrientationChange);
-    handleOrientationChange(); // Set initial orientation
-
-    return () => {
-      window.removeEventListener('resize', handleOrientationChange);
-    };
-  }, []);
 
   useEffect(() => {
     if (isOpen) {
@@ -56,10 +41,6 @@ const CameraPopup = ({ isOpen, onClose }) => {
             ref={webcamRef}
             screenshotFormat="image/jpeg"
             className="webcam"
-            style={{
-              transform: orientation === 'landscape' ? 'rotate(180deg)' : 'rotate(180deg)',
-              transformOrigin: 'center center'
-            }}
           />
         ) : (
           <img src={image} alt="Captured selfie" className="captured-image" />
